beff3e594b4da7ccbc8b2dcf2ca57c6be818c89c
ChuMutt <chufilthymutt@gmail.com>
2024-03-06 19:28:36 -0600

    ch3 cd database "cddb.lisp"

---
+++ ch2/hello-world.lisp
@@ -0,0 +1,24 @@
+; 
+;     |,\__/|
+;     |  o o|
+;     (   T )    Are we consing yet?
+;    .^`--^'^.
+;    `.  ;  .'
+;    | | | | |
+;   ((_((|))_))
+;              hjw
+; 
+; Dedicated output stream setup (port 46167)
+; Redirecting all output to this MREPL
+; SLY 1.0.43 (#<MREPL mrepl-1-1>)
+CL-USER> 
+Process sly-pty-1-1 killed
+
+; Quitting lisp
+; --------------------------------------------------------
+; Dedicated output stream setup (port 45183)
+; Redirecting all output to this MREPL
+; SLY 1.0.43 (#<MREPL mrepl-1-1>)
+CL-USER> (defun hello-world () (format t "Hello, world!"))
+HELLO-WORLD
+CL-USER> 
